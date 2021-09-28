60f05a000169be50b3b25af936fd0328d905f730
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-09-28 21:59:46 +0300

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import "antd/dist/antd.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pagination</h1>
+    </div>
+  );
+}
+
+export default App;
